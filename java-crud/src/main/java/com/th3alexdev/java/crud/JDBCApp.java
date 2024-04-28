8f376ee8ad4400677e1161cc0eb1b0c49e871b01
AleKs <alexanderfranciscog@gmail.com>
2024-04-28 16:01:07 -0400

    project created using netbeans

---
+++ java-crud/src/main/java/com/th3alexdev/java/crud/JDBCApp.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.th3alexdev.java.crud;
+
+/**
+ *
+ * @author PC
+ */
+public class JDBCApp {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
